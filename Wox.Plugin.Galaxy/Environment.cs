815c4d442ea8b203c0662c9f2ef7f9e6b6d1cd5e
thorio <thorio.git@gmail.com>
2020-01-21 21:07:58 +0100

    Add option to hide the launcher
    Brought to my attention by https://github.com/Megalex42, thanks!

--- Wox.Plugin.Galaxy/Environment.cs
+++ Wox.Plugin.Galaxy/Environment.cs
@@ -5,5 +5,7 @@ namespace Wox.Plugin.Galaxy
 	static class Environment
 	{
 		public static ConnectionStringSettings ConnectionString => Configuration.GetConnectionString("Galaxy2.0");
+		public static string ExePath => Configuration.GetKey("ExePath");
+		public static bool HideLauncher => bool.Parse(Configuration.GetKey("HideLauncher"));
 	}
 }
--- Wox.Plugin.Galaxy/GalaxyClient.cs
+++ Wox.Plugin.Galaxy/GalaxyClient.cs
@@ -1,11 +1,44 @@
-﻿namespace Wox.Plugin.Galaxy
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Wox.Plugin.Galaxy
 {
 	class GalaxyClient
 	{
+		[DllImport("user32.dll")]
+		private static extern bool ShowWindow(IntPtr hWnd, uint nCmdShow);
+
 		public static bool StartGame(string releaseKey)
 		{
-			System.Diagnostics.Process.Start(Configuration.GetKey("ExePath"), $"/command=runGame /gameId={releaseKey}");
+			Process.Start(Environment.ExePath, $"/command=runGame /gameId={releaseKey}");
+			if (Environment.HideLauncher)
+			{
+				HideClient();
+			}
 			return true;
+		}
+
+		public async static void HideClient()
+		{
+			var i = 5;
+			while (i-- > 0)
+			{
+				HideClient_Internal();
+				await Task.Delay(100);
+			}
+		}
+
+		private static bool HideClient_Internal()
+		{
+			var process = Process.GetProcessesByName("GalaxyClient").FirstOrDefault();
+			if (process != null)
+			{
+				return ShowWindow(process.MainWindowHandle, 0);
+			}
+			return false;
 		}
 	}
 }
